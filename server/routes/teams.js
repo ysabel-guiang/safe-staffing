e980ec34ff37ccebd6d4a1cd9bc57d1b6a62ecbc
Ysabel Regino <ysabelregino@yahoo.com>
2022-09-15 01:22:08 +1200

    fix nested promises with async await

--- server/routes/teams.js
+++ server/routes/teams.js
@@ -3,143 +3,108 @@ const router = express.Router()
 
 const db = require('../db/db')
 
-router.get('/', (req, res) => {
-  db.getAllTeams()
-    .then(teams => {
-      res.json(teams)
-    })
-    .catch(err => {
-      res.status(500).send(err.message)
-    })
-  }
-)
-
-router.get('/:teamId', (req,res) => {
-  const { teamId } = req.params
-  db.getEmployees(teamId)
-    .then(employees => {
-      res.json(employees)
-    })
-    .catch(err => {
-      res.status(500).send(err.message)
-    })
-})
-
-router.post('/', (req,res) => {
-  const { teamName, description, name, role, email, contact } = req.body
-  db.addTeam(teamName, description)
-  .then(id => { 
-    db.addEmployee(id[0], name, role, email, contact)
-    .then(() => {
-      res.status(200).send()
-    })
-    .catch(err => {
-      res.status(500).send(err.message)
-    })
-  })
-  .catch(err => {
+router.get('/', async (req, res) => {
+  try {
+    const teams = await db.getAllTeams()
+    res.json(teams)
+  } catch (err) {
     res.status(500).send(err.message)
-  })
+  }
 })
 
-router.post('/:teamId', (req,res) => {
-  const { teamId } = req.params
-  const { name, role, email, contact } = req.body
+router.get('/:teamId', async (req, res) => {
+  try {
+    const { teamId } = req.params
+    const employees = await db.getEmployees(teamId)
+    res.json(employees)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
 
-  db.addEmployee(teamId, name, role, email, contact)
+router.post('/', async (req, res) => {
+  try {
+    const { teamName, description, name, role, email, contact } = req.body
+    const newTeamId = await db.addTeam(teamName, description)[0]
+    await db.addEmployee(newTeamId, name, role, email, contact)
+    res.status(200).send()
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+router.post('/:teamId', async (req, res) => {
+  try {
+    const { teamId } = req.params
+    const { name, role, email, contact } = req.body
+    await db.addEmployee(teamId, name, role, email, contact)
+    res.status(200).send()
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+router.post('/:teamId/task', async (req, res) => {
+  try {
+    const { taskName, hoursWork, urgency, employees } = req.body
+
+    const taskId = await db.addTask(taskName, hoursWork, urgency)[0]
+    await db.addEmployeesAndTasks(taskId, employees)
+    res.status(200).send()
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+})
+
+router.patch('/:teamId/task', (req, res) => {
+  const { hoursWork, urgency, employees } = req.body
+  const hoursEach = findHours(hoursWork, urgency, employees)
+
+  Promise.all(
+    employees.map((employeeId) => db.updateEmployeeHours(employeeId, hoursEach))
+  )
     .then(() => {
       res.status(200).send()
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send(err.message)
     })
 })
 
-// router.get ('/:teamId/task', (req,res) => {
-//   res.send('hello')
-//   return null
-// })
-
-router.post('/:teamId/task', (req,res) => {
-  
-  const {taskName, hoursWork, urgency, employees} = req.body
-
-  db.addTask(taskName, hoursWork, urgency)
-    .then(taskId => {
-  
-      db.addEmployeesAndTasks(taskId[0], employees)
-        .then(() => {
-          res.status(200).send()
-        })
-      .catch(err => {
-        res.status(500).send(err.message)
-      })
-
-    })
-    .catch(err => {
-      res.status(500).send(err.message)
-    })
+router.get('/:teamId/:employeeId', async (req, res) => {
+  try {
+    const { employeeId } = req.params
+    const employee = await db.getEmployee(employeeId)
+    res.json(employee)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
 })
 
-router.patch('/:teamId/task', (req,res) => {
-  const {hoursWork, urgency, employees} = req.body
-  const hoursEach = findHours(hoursWork,urgency, employees)
+router.patch('/:teamId/:employeeId', async (req, res) => {
+  try {
+    const { employeeId } = req.params
+    const { name, role, email, contact } = req.body
 
-  Promise.all(employees.map(employeeId => (
-    db.updateEmployeeHours(employeeId,hoursWork)
-    )))
-    .then(() => {
-      res.status(200).send()
-    })
-    .catch(err => {
-      res.status(500).send(err.message)
-    })  
+    await db.updateEmployee(employeeId, name, role, email, contact)
+    res.status(200).send()
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
 })
 
-router.get('/:teamId/:employeeId', (req, res) => {
-  const { employeeId } = req.params
-  console.log(req.params)
-
-  db.getEmployee(employeeId)
-    .then(details => {
-      console.log('hello')
-      res.json(details)
-    })
-    .catch(err => {
-      res.status(500).send(err.message)
-    }) 
+router.delete('/:teamId/:employeeId', async (req, res) => {
+  try {
+    const { employeeId } = req.params
+    await db.deleteEmployee(employeeId)
+    res.status(200).send()
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
 })
-
-router.patch('/:teamId/:employeeId', (req, res) => {
-  const { employeeId } = req.params
-  const { name, role, email, contact } = req.body
-
-  db.updateEmployee(employeeId, name, role, email, contact)
-    .then(() => {
-      res.status(200).send()
-    })
-    .catch(err => {
-      res.status(500).send(err.message)
-    }) 
-})
-
-router.delete('/:teamId/:employeeId', (req, res) => {
-  const { employeeId } = req.params
-
-  db.deleteEmployee(employeeId)
-    .then(() => {
-      res.status(200).send()
-    })
-    .catch(err => {
-      res.status(500).send(err.message)
-    }) 
-})
-
 
 module.exports = router
 
-function findHours (hours, urgency, employeeArr) {
+function findHours(hours, urgency, employeeArr) {
   return Number((Number(hours) + Number(urgency)) / employeeArr.length)
 }
-
-
