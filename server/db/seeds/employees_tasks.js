4e821a7ff7fa3275931bc86304652f4ba17d21a4
Ysabel Regino <ysabelregino@yahoo.com>
2022-09-23 22:10:39 +1200

    update README

--- server/db/seeds/employees_tasks.js
+++ server/db/seeds/employees_tasks.js
@@ -1,11 +1,9 @@
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('employees_tasks').del()
+  return knex('employees_tasks')
+    .del()
     .then(function () {
       // Inserts seed entries
-      return knex('employees_tasks').insert([
-        {employee_id: 101, task_id: 6},
-
-      ]);
-    });
-};
+      return knex('employees_tasks').insert([{ employee_id: 201, task_id: 6 }])
+    })
+}
